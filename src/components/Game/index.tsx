cf31fd76b99a0b87a998074c53bd9baf725c9b82
jose.paulo <jose.paulo@ceuma.br>
2024-01-27 04:51:51 -0300

    save score indo

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,19 +1,37 @@
-// Game.tsx
 import React, { useState, useEffect } from 'react';
 import './style.css';
-import { useNavigate } from 'react-router-dom';
 import Progress from '../Progress';
 import FaceTable from '../FaceTable';
 import Timer from '../Timer';
 import { generateLevel } from '../../utils/generateLevel';
+import { formatTime } from '../../utils/formatTime';
 
 function Game() {
   const [currentLevel, setCurrentLevel] = useState<number[][]>([]);
   const [currentProgress, setCurrentProgress] = useState(-1);
   const [gameInProgress, setGameInProgress] = useState(true);
-  const navigate = useNavigate(); // Utiliza o hook useNavigate para redirecionamento
+  const [currentScreen, setCurrentScreen] = useState('playing');
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
+
+  const [playerName, setPlayerName] = useState('');
+
+  const handleInputChange = (e) => {
+    // Limita o comprimento do nome do jogador para 3 caracteres
+    const inputName = e.target.value.slice(0, 3);
+    setPlayerName(inputName);
+  };
 
   useEffect(() => {
+    // Auto submeter quando 3 caracteres são atingidos
+    if (playerName.length === 3) {
+      // onSubmit(playerName);
+      alert('mandei ' + playerName);
+    }
+  }, [playerName]);
+
+  useEffect(() => {
+    setStartTime(Date.now());
     generateNewLevel();
   }, []);
 
@@ -21,37 +39,71 @@ function Game() {
     setCurrentLevel(generateLevel());
     setCurrentProgress(currentProgress + 1);
     setGameInProgress(true);
+    setCurrentScreen('playing');
   };
 
   const handleCorrectShot = () => {
-    alert('certo!');
-    generateNewLevel();
+    setGameInProgress(false);
+    if (currentProgress >= 2) {
+      setEndTime(Date.now());
+      setCurrentScreen('score');
+    } else {
+      setTimeout(() => generateNewLevel(), 500);
+    }
   };
 
   const handleMistakenShot = () => {
     alert('errado!');
     setGameInProgress(false);
     setTimeout(() => {
-      navigate('/'); // Usa o navigate para redirecionamento
+      setCurrentScreen('menu');
     }, 1500);
   };
 
   return (
     <div className="vertical-container">
-      <div className="subcontainer-flex1">
-        <Timer bestTime={4500} gameInProgress={gameInProgress} />
-      </div>
-      <div className="subcontainer-flex3">
-        <FaceTable
-          currentLevel={currentLevel}
-          onCorrectShot={handleCorrectShot}
-          onMistakenShot={handleMistakenShot}
-          gameInProgress={gameInProgress}
-        />
-      </div>
-      <div className="subcontainer-flex1">
-        <Progress max={10} current={currentProgress} />
-      </div>
+      {currentScreen === 'playing' && (
+        <div>
+          screen playing:
+          <div className="subcontainer-flex1">
+            <Timer bestTime={4500} gameInProgress={gameInProgress} />
+          </div>
+          <div className="subcontainer-flex3">
+            <FaceTable
+              currentLevel={currentLevel}
+              onCorrectShot={handleCorrectShot}
+              onMistakenShot={handleMistakenShot}
+              gameInProgress={gameInProgress}
+            />
+          </div>
+          <div className="subcontainer-flex1">
+            <Progress max={10} current={currentProgress} />
+          </div>
+        </div>
+      )}
+
+      {currentScreen === 'score' && (
+        <div>
+          <input
+            style={{
+              border: 'none',
+              outline: 'none',
+              color: 'purple',
+              fontSize: '3.5em',
+              width: '2em', // Ajuste o valor conforme desejado
+              textTransform: 'uppercase',
+            }}
+            type="text"
+            id="playerName"
+            value={playerName}
+            onChange={handleInputChange}
+            maxLength="3"
+            autoFocus
+          />
+
+          <h2>Tempo: {formatTime(endTime - startTime)} </h2>
+        </div>
+      )}
     </div>
   );
 }
